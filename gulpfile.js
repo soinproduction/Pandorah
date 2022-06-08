2df5130251694d4cee96d86ccb9792cdd792f698
Andrey_Soin <soinproduction@gmail.com>
2022-06-08 21:35:55 +0300

    bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -128,7 +128,7 @@ gulp.task("html", () => {
 gulp.task("copy", () => {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
-      "source/img/**/*.{png,jpg,jpeg,svg,gif}",
+      "source/img/**/*.{png,jpg,jpeg,svg,gif,ico}",
       "source/*.ico"
     ], {
       base: "source"
